df199de750a822aafd0dcfdd080e63a100de016f
kulthu <106136463+kulthu@users.noreply.github.com>
2023-05-02 22:41:04 +0500

    4 zadanie

--- task_4/solution.js
+++ task_4/solution.js
@@ -1,6 +1,16 @@
 // Задание 4.1. Вывод карточек товаров
 
 function parseProducts(json) {
+    let data = JSON.parse(json);
+        return data.products;
+        }
+function renderProductsCards(json) {
+  clearProducts();
+  let products = parseProducts(json);
+  let length = products.length;
+  for (let i = 0; i < length; i++) {
+    addProduct(products[i]);
+  }
     // Аргументом функции является JSON
 
     // Преобразуйте строку json, переданную как аргумент функции,
