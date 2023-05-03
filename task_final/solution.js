23d64167af7983d8bed05f9251e5cec5c4622a50
kulthu <106136463+kulthu@users.noreply.github.com>
2023-05-03 21:56:38 +0500

    Update solution.js
    itogovaya rabota

--- task_final/solution.js
+++ task_final/solution.js
@@ -38,16 +38,17 @@ function sendRequest(name, phone, address, goods, sum) {
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i += 1) {
+        let good = {title: goods[i].title, count: goods[i].count};
+        data.goods.push(good);
     }
-
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
+    
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`;
+    data.order.sum = sum;
+    
+    data.client = name + ' ' + phone;
+    
+    let jsonData = JSON.stringify({data});
+    
     return jsonData;
 }
